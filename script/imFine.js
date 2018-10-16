93f84972d603a185ebc62ad7ee338dcf015c587d
Jerson Hatch <vote4chubs@hotmail.com>
2018-10-16 15:08:23 -0600

    a little more JS

--- script/imFine.js
+++ script/imFine.js
@@ -1,46 +1,84 @@
 // javascript stuff
 
+function goodiness() {
+
+    console.log("running goodiness");
+    var y = document.getElementById("goodGood");
+
+    if (y.style.display = "none") {
+        y.style.display = "block";
+console.log ("did the display change?")
+    };
+    console.log ("no display changed?");
+};
 // button I'm ok
-function imGood (){
+function imGood() {
     alert("sends alert to whoever is watching 'I'm doing just Fine today!'")
     // if they have been sent a notification - it will erase notification;
-    var x=document.getElementById("alertMe");
-    if (x.style.display==="block") {
-        x.style.display="none";
-    };
 
+
+
+
+    var x = document.getElementById("alertMe");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+        console.log("close alert");
+        goodiness();
+    };
+    goodiness();
+    console.log("nothing happend");
 };
 
 
+
+
 // button Need help
-function helpMe () {
+function helpMe() {
     alert("send an alert to whoever is watching, 'please call for Help'")
     // add questions
     // do you want me to call 911?
     // Family member needs to call?
-    
+
 };
 
-function checkUp () {
+
+
+function checkUp() {
 
     var x = document.getElementById("alertMe");
-    if (x.style.display==="block"){
-        x.style.display="none";
+    if (x.style.display === "block") {
+        x.style.display = "none";
         // x.style.border="2pt solid black"
     } else {
-        x.style.display="block";
+        x.style.display = "block";
     };
     alert("send an alert to user 'Are you OK?' ");
 
 };
 
 
-function addNew () {
-    alert ("This will allow you to add another person to your watchlist");
-}
+function addNew() {
+    alert("This will allow you to add another person to your watchlist");
+    var x = document.getElementById("inputAdd");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    };
 
-
-function deleteMe () {
-    alert("delete someone from your watchlist, will show a list of people");
 };
 
+
+function deleteMe() {
+    event.preventDefault()
+    alert("delete someone from your watchlist, will show a list of people");
+    var y = document.getElementById("inputDelete");
+    if (y.style.display === "block") {
+        y.style.display = "none";
+        console.log("first ran");
+    } else {
+        console.log("else ran");
+        y.style.display = "block";
+    };
+    console.log("done");
+};
